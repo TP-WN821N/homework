3a6dc4a2e20eaafca4adc6254637c839c1fffbbd
TP-WN821N <azikmelor7705@gmail.com>
2024-07-02 14:36:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// 1. uchta sondan eng kattasini chiqaruvchi algaritm
+// let a = prompt("a=")
+// let b = prompt("b=")
+// let c = prompt("c=")
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > c) {
+//   console.log(b);
+// } else {
+//   console.log(c);
+// }
+
+
+// 2. foydalanuvchi mablag’ini kiritadi va mehmonhonada olinadigan hona turini ayting 1000 - lux | 500 - yarim lux yoki 500dan 200 gacha bo’lsa odiy hona 200 dan kam bo’lsa - joy yo’q
+// let sum = prompt("Qancha pulingiz bor")
+// if (sum >= 1000) {
+//   console.log("Marxamad Lux xonaga");
+// } else if (sum >= 500) {
+//   console.log("Marxamad yarim Lux xonaga");
+// } else if (sum >= 200) {
+//   console.log("Marxamad oddiy xonaga");
+// } else {
+//   console.log("Pulingiz kam");
+// }
+// 3. foydalanucvchi son kiritadi agar son 3 ga bo’linsa fizz 5 ga bo’lisa bass agar 3 ham 5 ga bo’linsa fizz-bazz
+// let a = prompt("a=")
+// if (a % 15 == 0) {
+//   console.log('fizz-bazz');
+// } else if (a % 5 == 0) {
+//   console.log('fizz');
+// }else if (a % 3 == 0) {
+//   console.log('bazz');
+// }
+// 4. uchta sondan eng kichigini chiqaruvchi algaritm
+// let a = prompt("a=")
+// let b = prompt("b=")
+// let c = prompt("c=")
+// if (a < b && a < c) {
+//   console.log(a);
+// } else if (b < c) {
+//   console.log(b);
+// } else {
+//   console.log(c);
+// }
